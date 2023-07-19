258aa4f0a1fcca5117a5d44062a90882d9424548
Ekrem Koseli <ekoseli2@illinois.edu>
2023-07-19 05:25:05 -0500

    React Migrations

---
+++ src/weatherForcast.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherForecastPreview from "./WeatherForecastPreview";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleForecastResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+  }
+
+  if (loaded && props.city === forecast.city.name) {
+    return (
+      <div className="WeatherForecast row">
+        <WeatherForecastPreview data={forecast.list[0]} />
+        <WeatherForecastPreview data={forecast.list[1]} />
+        <WeatherForecastPreview data={forecast.list[2]} />
+        <WeatherForecastPreview data={forecast.list[3]} />
+        <WeatherForecastPreview data={forecast.list[4]} />
+        <WeatherForecastPreview data={forecast.list[5]} />
+      </div>
+    );
+  } else {
+    let apiKey = "c90c4a455e1973704f6aa4951d1a6b90";
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleForecastResponse);
+
+    return null;
+  }
+}
